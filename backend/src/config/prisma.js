c5f058bf60d1fbc3fbb7c0e4f8da25385feb6bf1
Gilbert Reyno <gilbertreynojr@gmail.com>
2026-07-02 20:25:20 -0700

    before full prisma implimentation

---
+++ backend/src/config/prisma.js
@@ -0,0 +1,11 @@
+import pkg from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { PrismaClient } = pkg;
+const prisma = new PrismaClient({
+  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
+});
+export default prisma;
+// export const sql = prisma.$queryRaw;
